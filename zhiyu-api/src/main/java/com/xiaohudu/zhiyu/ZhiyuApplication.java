82612cbc544cbcdcb0f46508b6d6b1df8815d128
Mu Haibao <mhbseal@163.com>
2024-08-27 17:12:37 +0800

    up

---
+++ zhiyu-api/src/main/java/com/xiaohudu/zhiyu/ZhiyuApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohudu.zhiyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhiyuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhiyuApplication.class, args);
+	}
+
+}
---
+++ zhiyu-api/src/test/java/com/xiaohudu/zhiyu/ZhiyuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohudu.zhiyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhiyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
